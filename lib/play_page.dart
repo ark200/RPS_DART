7fab7a5202cfb933fb25ea8e167341dec2e406b5
arunr <arunroyk200@gmail.com>
2024-04-19 02:28:39 +0100

    Initial commit

---
+++ lib/play_page.dart
@@ -0,0 +1,127 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:js';
+import 'package:flutter/material.dart';
+import 'package:test3/GameUtils.dart';
+import 'package:test3/database_helper2.dart';
+import 'package:test3/gameutils.dart';
+
+import 'database_helper.dart';
+
+class PlayPage extends StatefulWidget {
+  final int winningScore;
+
+  PlayPage({required this.winningScore});
+
+  @override
+  _PlayPageState createState() => _PlayPageState();
+}
+
+class _PlayPageState extends State<PlayPage> {
+  GameUtils gameUtils;
+  int myScore = 0;
+  int oppScore = 0;
+  String result = '';
+  final dbHelper = Database2Helper.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    gameUtils = GameUtils(widget.winningScore, context);
+  }
+
+  void updateUI(String newResult, int newMyScore, int newOppScore) {
+    setState(() {
+      result = newResult;
+      myScore = newMyScore;
+      oppScore = newOppScore;
+    });
+
+    if (myScore >= widget.winningScore || oppScore >= widget.winningScore) {
+      saveRecord();
+      showDialog();
+    }
+  }
+
+  Future<void> saveRecord() async {
+    final db = await dbHelper.database;
+    final date = DateTime.now();
+
+
+    final record = {
+      Database2Helper.date: date,
+      Database2Helper.myScore: myScore,
+      Database2Helper.oppScore: oppScore,
+      Database2Helper.result: result,
+    };
+
+    await db.insert(Database2Helper.table, record);
+  }
+
+  void showDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Game Over'),
+          content: Text('Your score: $myScore, Opponent score: $oppScore'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.pushReplacementNamed(context, '/homepage');
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              result,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                final newResult = gameUtils.play('rock');
+                updateUI(newResult, gameUtils.getMyScore(), gameUtils.getOppScore());
+              },
+              child: Text('Rock'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                final newResult = gameUtils.play('paper');
+                updateUI(newResult, gameUtils.getMyScore(), gameUtils.getOppScore());
+              },
+              child: Text('Paper'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                final newResult = gameUtils.play('scissors');
+                updateUI(newResult, gameUtils.getMyScore(), gameUtils.getOppScore());
+              },
+              child: Text('Scissors'),
+            ),
+            SizedBox(height: 20),
+            Text('User Score: $myScore'),
+            Text('Opponent Score: $oppScore'),
+          ],
+        ),
+      ),
+    );
+  }
+}
